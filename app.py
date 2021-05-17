3384f9fb43ec04ccfd4aa1c27f73c7060ace43eb
JankaGramofonomanka <tsrn666@gmail.com>
2021-05-17 22:41:59 +0200

    Add test endpoints

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import os
+
+from flask import Flask, make_response
+import pymongo
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
@@ -7,6 +11,30 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello!"
+
+
+@app.route("/db_test")
+def db_test():
+
+    try:
+        cluster = MongoClient(os.environ["MONGODB_URI"])
+
+        db = cluster["notifai-task-db"]
+        col = db["test-collection"]
+
+        col.update_many(
+            {"name": "JOOOHN", "surname": "CENAAAAA!!!!!"}, 
+            {"$inc": {"score": 5000}}
+        )
+
+        return "success"
+    
+    except Exception:
+        return make_response("internal error", 500)
+
+@app.route("/env_test")
+def env_test():
+    return os.environ["EXAMPLE_ENV_VAR"]
 
 
 
