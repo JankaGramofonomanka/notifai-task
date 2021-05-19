4a99599ed890bad1f8c3840d93fe60332056cfac
JankaGramofonomanka <tsrn666@gmail.com>
2021-05-19 22:24:11 +0200

    Remove the unused `convert` decorator

--- decorators.py
+++ decorators.py
@@ -16,41 +16,6 @@ import lib
 
 
 # -----------------------------------------------------------------------------
-def convert(**kwtypes : type):
-    """
-    Returns a decortor that converts string key word arguments of a function 
-    to given types (for example, if `kwtypes[param] == int`, then the 
-    parameter `param` will be converted to `int`.)
-
-    If it is impossible, the decorated function will return a 400 response.
-    """
-    
-    def decorator(func):
-        
-        @wraps(func)
-        def decorated(**kwargs : str):
-            
-            for arg, type_of_arg in kwtypes.items():
-
-                try:
-                    kwargs[arg] = type_of_arg(kwargs[arg])
-
-                except ValueError:
-                    return make_response(
-                        f"parameter {arg} should be of type {type}",
-                        400
-                    )
-
-            return func(**kwargs)
-        
-        return decorated
-    
-    return decorator
-
-
-
-
-# -----------------------------------------------------------------------------
 def require_token(func):
     """
     Checks if a valid acces token is provided, 
