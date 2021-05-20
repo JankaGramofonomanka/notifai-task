498ca1f0962a311774c015d26b6a09eca9d8c6e2
JankaGramofonomanka <tsrn666@gmail.com>
2021-05-20 15:26:35 +0200

    Move some constants to environment
    Moved `DATABASE_NAME`, `POST_COLLECTION_NAME`, `TOKEN_VALIDITY_TIME` to
    environment, that is, they are now expected to be environment variables.

--- constants.py
+++ constants.py
@@ -1,21 +1,21 @@
 import os
 
-MONGODB_URI = os.environ["MONGODB_URI"]
-SECRET_KEY = os.environ["SECRET_KEY"]
-PASSWORD = os.environ["PASSWORD"]
+MONGODB_URI             = os.environ["MONGODB_URI"]
+SECRET_KEY              = os.environ["SECRET_KEY"]
+PASSWORD                = os.environ["PASSWORD"]
+DATABASE_NAME           = os.environ["DATABASE_NAME"]
+POST_COLLECTION_NAME    = os.environ["POST_COLLECTION_NAME"]
 
-DATABASE_NAME = "notifai-task-db"
-POST_COLLECTION_NAME = "posts"
+TOKEN_VALIDITY_TIME = int(os.environ["TOKEN_EXP_TIME"])
 
 MAX_POST_LENGTH = 160
-TOKEN_VALIDITY_TIME = 30
 
-DATABASE_FORMAT_ERROR_MSG = "Data is corrupt."
-INVALID_CONTENT_MSG = "Request does not contain a valid post."
-POST_ALREDY_EXISTS_MSG = "Post alredy exists."
-POST_NOT_FOUD_MSG = "Post does not exist."
-NO_PASSWORD_MSG = "No password provided."
-INVALID_ID_MSG = "The Id of the post is invalid."
+DATABASE_FORMAT_ERROR_MSG   = "Data is corrupt."
+INVALID_CONTENT_MSG         = "Request does not contain a valid post."
+POST_ALREDY_EXISTS_MSG      = "Post alredy exists."
+POST_NOT_FOUD_MSG           = "Post does not exist."
+NO_PASSWORD_MSG             = "No password provided."
+INVALID_ID_MSG              = "The Id of the post is invalid."
 
 POST_CREATED_MSG = "The post has been created"
 POST_UPDATED_MSG = "The post has been updated"
